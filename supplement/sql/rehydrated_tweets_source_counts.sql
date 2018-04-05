aeed07fee030618fa8855ae2e7ee98cbd58a43c4
Dmitrijs Milajevs <dimazest@gmail.com>
2018-04-05 21:14:20 +0000

    Tracked statistics

--- supplement/sql/rehydrated_tweets_source_counts.sql
+++ supplement/sql/rehydrated_tweets_source_counts.sql
@@ -10,7 +10,10 @@ order by count(*) desc
 limit 10
 ;
 
-select s.*, t.features#>>'{languages,0}' lang
+select
+  s.*,
+  t.features#>>'{languages,0}' lang,
+  jsonb_array_length(features->'screen_names') > 1 is_tracked
 into temporary table _
 from lv2_rehydrated_tweets t
 right join top_sources s on t.features#>>'{filter,source}' = s.source
@@ -19,18 +22,25 @@ where t.collection = 'lv2'
 
 select distinct on (tweet_count, source, lang)
   source, tweet_count total_count, share total_share, lang,
-  count(*) over (partition by source, lang order by source, lang) source_lang_count,
-  count(*) over (partition by source order by source) source_count
+  count(*) over source_lang source_lang_count,
+  count(*) over source source_count,
+  count(nullif(is_tracked, false)) over source_lang tracked_source_lang_count,
+  count(nullif(is_tracked, false)) over source tracked_source_count
 into temporary table __
 from _
 where lang in ('lv', 'ru', 'en')
+window
+  source as (partition by source order by source),
+  source_lang as (partition by source, lang order by source, lang)
 ;
 
 select
   *,
   to_char(100.0 * source_lang_count / total_count, '990D0%') source_lang_share,
   total_count - source_count other_lang_count,
-  to_char(100 - 100.0 * source_count / total_count, '990D0%') other_lang_share
+  to_char(100 - 100.0 * source_count / total_count, '990D0%') other_lang_share,
+  to_char(100.0 * tracked_source_count / total_count, '990D0%') tracked_source_share,
+  to_char(100.0 * tracked_source_lang_count / source_lang_count, '990D0%') tracked_source_lang_share
 into temporary table result
 from __
 order by total_count desc, source, lang
