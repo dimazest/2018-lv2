1d1abf81c0c25ee8ad2bd4ee2dd36f424ee0a9d7
Dmitrijs Milajevs <dimazest@gmail.com>
2018-04-09 19:35:07 +0000

    Tweet count by day and language

--- supplement/sql/rehydrated_tweets_count_by_day.sql
+++ supplement/sql/rehydrated_tweets_count_by_day.sql
@@ -1,8 +1,11 @@
-select date_trunc('day', created_at) "day", count(*)
+select
+  date_trunc('day', created_at) "day",
+  features#>>'{languages,0}' lang,
+  count(*)
 into temporary table result
 from lv2_rehydrated_tweets
-group by "day"
-order by "day"
+group by "day", lang
+order by "day", count(*), lang
 ;
 
 \copy (select * from result) to stdin with csv header
