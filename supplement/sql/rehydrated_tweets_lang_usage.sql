5bde05f92c4981bcf046b8384bc06a9e57ed59a7
Dmitrijs Milajevs <dimazest@gmail.com>
2018-04-09 21:00:28 +0000

    Langugage usage

---
+++ supplement/sql/rehydrated_tweets_lang_usage.sql
@@ -0,0 +1,31 @@
+begin;
+
+drop materialized view if exists lv2_lang_usage;
+
+create materialized view lv2_lang_usage as
+with _ as (
+  select
+    (features#>>'{user_info,screen_name_id}')::bigint screen_name_id,
+    features#>>'{languages,0}' lang
+  from lv2_rehydrated_tweets
+),
+__ as (
+  select
+    distinct (screen_name_id)
+    screen_name_id,
+    count(nullif(lang='lv', false)) over sn as lv,
+    count(nullif(lang='ru', false)) over sn as ru,
+    count(nullif(lang='en', false)) over sn as en,
+    count(*) over sn as total
+  from _
+  window sn as (partition by screen_name_id order by screen_name_id)
+  order by total desc
+)
+select *
+from __
+where total >= 50
+;
+
+commit;
+
+\copy (select lv, ru, en, total from lv2_lang_usage) to stdin with csv header
