967a60e76a7fd74004bce05706a4e7217069429d
Dmitrijs Milajevs <dimazest@gmail.com>
2018-04-10 17:53:16 +0000

    Tweets starting from April 15 2017

--- supplement/sql/rehydrated_tweets.sql
+++ supplement/sql/rehydrated_tweets.sql
@@ -1,7 +1,7 @@
 begin;
 
-drop materialized view if exists lv2_rehydrated_tweets
-;
+drop materialized view if exists lv2_rehydrated_tweets;
+drop materialized view if exists lv2_lang_usage;
 
 create materialized view lv2_rehydrated_tweets  as
 select *
@@ -9,6 +9,7 @@ from tweet
 where
     collection = 'lv2'
 and hydrated_at = (select max(hydrated_at) from tweet where collection = 'lv2')
+and created_at >= '2017-04-15'
 and not features->'country_codes' ? 'SE'
 ;
 
